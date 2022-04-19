afa6f38f58810a71bdd967588999796591c452a0
Yana-Lesina <yanalesina3@gmail.com>
2022-04-19 17:37:57 +0300

    webpack..

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: __dirname + '/dist',
-//   },
-// };
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/dist',
+  },
+};
